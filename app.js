fb860142eae6093f005b07ff2f515d62fafec38d
unknown <royharshit812@gmail.com>
2023-02-12 12:01:49 +0530

    Added security packages

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const route = require('./routers/route');
 const connectDB = require('./db/connection');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimiter = require('express-rate-limit');
 require('dotenv').config();
 
 app.use(express.json());
@@ -10,6 +13,14 @@ app.use(express.static('./public'));
 const port = 3000;
 
 app.use('/',route);
+
+app.set('trust proxy',1);
+app.use(rateLimiter({
+  windowMs : 15 * 60 * 1000,
+max : 100
+}));
+app.use(helmet());
+app.use(cors());
 
 const start = async () => {
     try {
